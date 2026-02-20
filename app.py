c03498218211c5c485b90d89929b4c2fec28b3c6
Piotr Zurkowski <piotr.zurowski159@gmail.com>
2026-02-20 15:37:33 +0100

    ann classification streamlit app

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,   LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
+
+model = tf.keras.models.load_model('mode.h5')
+
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+with open('label_encoder_gender.pkl', 'rb') as f:
+    label_encoder_gender = pickle.load(f)
+
+with open('onehot_encoder_geo.pkl', 'rb') as f:
+    onehot_encoder_geo = pickle.load(f)
+
+st.title('Customer Churn Prediction')
+
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 100, 30)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+input_scaled = scaler.transform(input_data)
+
+prediction = model.predict(input_scaled)
+prediction_proba = prediction[0][0]
+
+st.write(f"prediction= {prediction}")
+st.write(f"prediction probability= {prediction_proba}")
